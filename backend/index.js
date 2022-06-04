7d83ac13aafcb750fbd911663f49112462eb35f9
Vinh Pham <VinhP@sngoodwill.org>
2022-06-04 11:33:26 -0700

    add mongoose, cors, gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,20 @@
 require('dotenv').config()
 const express = require('express')
 const port = process.env.PORT
+const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
+
+
+// MIDDLE WARE
+app.use(cors());
+app.use(express.json()); // to parse request body json
+
+// Mongoose
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    (e) => { console.log(e) }
+  )
 
 app.get('/', (req, res) => {
     res.json('Server is working')
